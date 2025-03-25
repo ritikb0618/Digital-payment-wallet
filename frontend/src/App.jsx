6513d88ea7d03f0298d165cead7770572cc90424
ritikb0618 <ritikb0618@gmail.com>
2025-03-26 00:54:14 +0530

    signin,signup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+
+import PrivateRoute from './pages/PrivateRoute';
+import { AuthProvider,useAuth } from './context/authContext';
+import SignUp from './pages/Signup'
+import SignIn from './pages/Signin'
---
+++ frontend/src/store/atom/SideBarAtom.jsx
@@ -0,0 +1,6 @@
+import {atom} from 'recoil'
+const SideBarOpen=atom({
+    key:'SideBarOpen',
+    default:false
+});
+export {SideBarOpen}
